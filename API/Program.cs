a17a4b64b91ee65ed4aa05eff2fc32d99fe423fd
Johnny Casarez <johnny.jc.casarez@gmail.com>
2023-08-03 10:57:10 -0500

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -14,7 +14,11 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy", policy =>{
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline. This is referred to as middleware. 
@@ -25,6 +29,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
