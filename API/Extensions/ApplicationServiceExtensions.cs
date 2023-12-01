10d6830f096431e475b08f3ed4ae1234b81b1a87
Johnny Casarez <johnny.jc.casarez@gmail.com>
2023-12-01 15:05:28 -0600

    Email Confirmation complete

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -8,6 +8,7 @@ using FluentValidation.AspNetCore;
 using Application.Interfaces;
 using Infrastructure.Security;
 using Infrastructure.Photos;
+using Infrastructure.Email;
 
 namespace API.Extensions
 {
@@ -74,6 +75,7 @@ namespace API.Extensions
             services.AddHttpContextAccessor();
             services.AddScoped<IUserAccessor, UserAccessor>();
             services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            services.AddScoped<EmailSender>();
             services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
             services.AddSignalR();
 
