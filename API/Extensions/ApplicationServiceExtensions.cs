7812e2b1cc47828db5397a93d478c476242fc07b
Johnny Casarez <johnny.jc.casarez@gmail.com>
2023-12-01 12:41:55 -0600

    Refresh Token Complete

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -63,6 +63,7 @@ namespace API.Extensions
                     policy.AllowAnyHeader()
                         .AllowAnyMethod()
                         .AllowCredentials()
+                        .WithExposedHeaders("WWW-Authenticate", "Pagination")
                         .WithOrigins("http://localhost:3000", "https://localhost:3000");
                 });
             });
--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -33,7 +33,7 @@ axios.interceptors.response.use(async response => {
     }
     return response;
 }, (error: AxiosError) => {
-    const {data, status, config} = error.response as AxiosResponse;
+    const {data, status, config, headers} = error.response as AxiosResponse;
     switch (status) {
         case 400:
             if(config.method === 'get' && Object.prototype.hasOwnProperty.call(data.errors, 'id')) {
@@ -53,7 +53,12 @@ axios.interceptors.response.use(async response => {
             }
             break;
         case 401:
-            toast.error('unauthorised')
+            if (status === 401 && headers['www-authenticate']?.startsWith('Bearer error="invalid_token"')) {
+                store.userStore.logout();
+                toast.error('Session expired - please login again');
+            } else {
+                toast.error('unauthorised')
+            }
             break;
         case 403:
             toast.error('forbidden')
@@ -93,7 +98,8 @@ const Account = {
     current: () => request.get<User>('/account'),
     login: (user: UserFormValues) => request.post<User>('/account/login', user),
     register: (user: UserFormValues) => request.post<User>('/account/register', user),
-    fbLogin: (accessToken: string) => request.post<User>(`/account/fbLogin?accessToken=${accessToken}`, {})
+    fbLogin: (accessToken: string) => request.post<User>(`/account/fbLogin?accessToken=${accessToken}`, {}),
+    refreshToken: () => request.post<User>('/account/refreshToken', {})
 }
 
 const Profiles = {
